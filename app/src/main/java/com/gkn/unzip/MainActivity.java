a4faa8332ec1d02079b0a6959a7f1adf7c3f60d5
Girish K.N <girish.kn@robosoftin.com>
2014-12-09 14:54:38 +0530

    UnZip using zip4j library

--- app/src/main/java/com/gkn/unzip/MainActivity.java
+++ app/src/main/java/com/gkn/unzip/MainActivity.java
@@ -2,16 +2,41 @@ package com.gkn.unzip;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.gkn.zip4j.ExtractZipFile;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
+import java.io.File;
+
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String zipFileName = "1415644062";
+
+        String zipFilePath = "/sdcard/" + zipFileName + ".zip";
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            String unZipPath = getExternalFilesDir(null).getPath();
+
+            boolean result = ExtractZipFile.extractZipFile(zipFilePath, unZipPath);
+            if (result) {
+                Log.d(TAG, "Zip file " + zipFilePath + "extracted to " + unZipPath);
+            } else {
+                Log.d(TAG, "Unzip Zip file " + zipFilePath + "Failed!");
+            }
+        }
     }
 
 
